3aa871b977d5bc1f8da7972435338bdc22fa54e8
AnjuBala <anjubudhwan@gmail.com>
2019-08-22 10:20:24 +0200

    redux-saga added

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,18 @@ import rootReducer from './rootReducer';
 import { persistStore } from 'redux-persist';
 // Logger with default options
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+//import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
-const middlewares = [thunk];
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+// then run the saga
+//sagaMiddleware.run();
+
 export const persistor = persistStore(store);
