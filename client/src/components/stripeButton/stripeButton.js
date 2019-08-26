082b03581a24053b94058e7335eb6a33263714a9
AnjuBala <anjubudhwan@gmail.com>
2019-08-26 12:23:17 +0200

    implemented stripe backed payment  with node js server

--- client/src/components/stripeButton/stripeButton.js
+++ client/src/components/stripeButton/stripeButton.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_reQMs5SU2cqEBgyTHdAlwmQS00Lc5KtSzF';
 
-  const onToken = () => {
-    alert('payment Successfull');
+  const onToken = token => {
+    axios({
+      method: 'post',
+      url: 'payment',
+      data: {
+        token,
+        amount: priceForStripe
+      }
+    })
+      .then(response => {
+        alert('succesful payment');
+      })
+      .catch(error => {
+        console.log('Payment Error: ', JSON.parse(error));
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
   };
   return (
     <StripeCheckout
